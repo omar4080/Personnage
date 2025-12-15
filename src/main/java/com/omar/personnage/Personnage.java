3f6430d0541080e5d550bf1a14f41bb5b0048a45
omar4080 <ombouaoud@gmail.com>
2025-12-15 22:42:56 +0100

    on corige le code pour que le test pass

--- src/main/java/com/omar/personnage/Personnage.java
+++ src/main/java/com/omar/personnage/Personnage.java
@@ -1,16 +1,14 @@
 package com.omar.personnage;
 
 public class Personnage {
-    String orientation;
-    Personnage(){
-
-        this.orientation = "nord";
-    }
+    String orientation="nord";
     public String tourner(int fois){
         if(fois==1){
             orientation="est";
         }else if(fois==2){
             orientation="sud";
+        } else if (fois==3) {
+            orientation="ouest";
         }
         return orientation;
     }
